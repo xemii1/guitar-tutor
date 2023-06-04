8ef88f7adc46fcc5b83c5d72cd3b8b6562d52a25
Chelyshkov Sergey <xemii@yandex.ru>
2023-06-04 13:14:05 +0300

    chore: connect pitch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,51 @@
 import React from 'react';
+import aubio from 'aubiojs';
+
+const start = () => {
+    const bufferSize = 4096;
+    const audioContext = new window.AudioContext();
+    const analyser = audioContext.createAnalyser();
+    const scriptProcessor = audioContext.createScriptProcessor(bufferSize, 1, 1);
+    scriptProcessor.connect(audioContext.destination);
+
+    aubio().then(({Pitch}) => {
+        console.log('aubio then')
+        const pitchDetector = new Pitch(
+            "default",
+            bufferSize,
+            1,
+            audioContext.sampleRate
+        );
+
+        navigator.mediaDevices
+            .getUserMedia({audio: true})
+            .then(function (stream) {
+                console.log('navigator mediaDevices')
+                audioContext.createMediaStreamSource(stream).connect(analyser);
+                analyser.connect(scriptProcessor);
+                scriptProcessor.connect(audioContext.destination);
+
+                scriptProcessor.addEventListener("audioprocess", function (event) {
+                    console.log('audioprocess');
+                    const data = event.inputBuffer.getChannelData(0);
+                    const frequency = pitchDetector.do(data);
+                    console.log('audioprocess f', frequency);
+
+                    if (frequency) {
+                        console.log(frequency.toFixed(1) + " Hz");
+                    }
+                });
+            })
+            .catch(function (error) {
+                alert(error.name + ": " + error.message);
+            });
+    });
+}
 
 function App() {
-  return (
-    <div />
-  );
+    return (
+        <button onClick={() => start()}>TEST</button>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import './polyfills';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
@@ -5,9 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
